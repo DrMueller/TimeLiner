13b59b955defc14cd43b5f89b23a8e435bf08c68
Matthias Müller <MatthiasM@Live.de>
2019-09-21 14:04:08 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/shared/shell/app-main/app.module';
+import { AppModule } from './app/core/shell/app-main/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
