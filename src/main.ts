56057dc331853cf4629244c20ad7ee65b054db88
Matthias Müller <MatthiasM@Live.de>
2019-09-21 12:57:06 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/shell/app-main/app.module';
 import { environment } from './environments/environment';
 
+// tslint:disable-next-line: ordered-imports
+import 'zone.js/dist/zone';
+
 if (environment.production) {
   enableProdMode();
 }
