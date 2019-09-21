26f17a1cd7d139bd67e39ca2bbf15e9bc3279952
Matthias Müller <MatthiasM@Live.de>
2019-09-21 13:10:28 +0200

    WIP

--- src/vss/vss-init/init-vss-angular.js
+++ src/vss/vss-init/init-vss-angular.js
@@ -1,31 +1,45 @@
 function initialize(explicitNotifyLoaded, usePlatformStyles, usePlatformScripts, afterSdkReadyCallback) {
-    appendScript('../../lib/VSS.SDK.min.js').onload = function () {
-        VSS.init({
-            explicitNotifyLoaded: explicitNotifyLoaded || false,
-            usePlatformStyles: usePlatformStyles || false,
-            usePlatformScripts: usePlatformScripts || false
-        });
+  appendScript('../../lib/VSS.SDK.min.js').onload = function () {
+    VSS.init({
+      explicitNotifyLoaded: explicitNotifyLoaded || false,
+      usePlatformStyles: usePlatformStyles || false,
+      usePlatformScripts: usePlatformScripts || false
+    });
 
-        VSS.ready(function () {
-            appendScript('../../angular-app/runtime-es5.js');
-            appendScript('../../angular-app/runtime-es2015.js');
-            appendScript('../../angular-app/polyfills-es5.js');
-            appendScript('../../angular-app/polyfills-es2015.js');
-            appendScript('../../angular-app/styles.css');
-            appendScript('../../angular-app/main-es5.js');
-            appendScript('../../angular-app/main-es2015.js');
+    VSS.ready(function () {
+      if (checkIfBrowserSupportsEs2015()) {
+        appendScript('../../angular-app/runtime-es2015.js');
+        appendScript('../../angular-app/polyfills-es2015.js');
+        appendScript('../../angular-app/main-es2015.js');
+      } else {
+        appendScript('../../angular-app/runtime-es5.js');
+        appendScript('../../angular-app/main-es5.js');
+        appendScript('../../angular-app/polyfills-es5.js');
+      }
 
-            if (afterSdkReadyCallback) {
-                afterSdkReadyCallback();
-            }
-        });
+      appendScript('../../angular-app/styles.css');
 
-    };
+      if (afterSdkReadyCallback) {
+        afterSdkReadyCallback();
+      }
+    });
 
-    function appendScript(scriptSource) {
-        let scriptTag = document.createElement('script');
-        scriptTag.src = scriptSource;
-        document.head.appendChild(scriptTag);
-        return scriptTag;
+  };
+
+  function checkIfBrowserSupportsEs2015() {
+    try {
+      new Function("(a = 0) => a");
+      return true;
     }
+    catch (err) {
+      return false;
+    }
+  };
+
+  function appendScript(scriptSource) {
+    let scriptTag = document.createElement('script');
+    scriptTag.src = scriptSource;
+    document.head.appendChild(scriptTag);
+    return scriptTag;
+  }
 };
