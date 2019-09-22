8d1251198d4ca6edae2988b38a175551b476832b
Matthias Müller <MatthiasM@Live.de>
2019-09-22 11:04:37 +0200

    WIP

--- src/app/core/vss/services/work-item.repo.ts
+++ src/app/core/vss/services/work-item.repo.ts
@@ -13,14 +13,38 @@ import { WorkItem } from '../models';
 export class WorkItemRepo {
   public loadAllAsync(dateFieldName: string): Promise<WorkItem[]> {
     return new Promise((resolve: (value?: WorkItem[]) => void, _: any) => {
+
+      VSS.require(
+        ['ReleaseManagement/Core/RestClient',
+          'ReleaseManagement/Core/Contracts'], (TFS_RM_RestClient: any, contracts: any) => {
+            try {
+              const tra = TFS_RM_RestClient.getClient()
+                .getReleaseDefinitions(
+                  1,
+                  null,
+                  contracts.ReleaseDefinitionExpands.Environments);
+
+              // tslint:disable-next-line: no-debugger
+              debugger;
+              console.log(tra);
+              // tslint:disable-next-line: no-debugger
+            } catch (er) {
+              debugger;
+              console.log(er);
+            }
+          });
+
       VSS.require(['VSS/Service', 'TFS/WorkItemTracking/RestClient'], async (VSS_Service: any, TFS_Wit_WebApi: any) => {
         try {
           const witClient = <CommonMethods4To5>VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
-          const nativeWorkItems = await witClient.getWorkItems([424, 1074]);
+          witClient.getWorkItems([424, 1074]).then(wis => {
+            // tslint:disable-next-line: no-debugger
+            debugger;
+            const workItems = wis.map(nw => new WorkItem(nw.id, nw.fields['title'], nw.fields['date']));
+            resolve(workItems);
+          });
           // tslint:disable-next-line: no-debugger
-          debugger;
-          const workItems = nativeWorkItems.map(nw => new WorkItem(nw.id, nw.fields['title'], nw.fields['date']));
-          resolve(workItems);
+
         } catch (er) {
           // tslint:disable-next-line: no-debugger
           debugger;
