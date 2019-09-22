ba41e37a913c7b3e34703a50b6b21d2bbbbc5148
Matthias Müller <MatthiasM@Live.de>
2019-09-22 18:15:04 +0200

    WIP

--- src/app/core/vss/services/work-item.repo.ts
+++ src/app/core/vss/services/work-item.repo.ts
@@ -2,22 +2,21 @@
 /// <reference path="../../../../../node_modules/vss-web-extension-sdk/typings/VSS.SDK.d.ts" />.
 
 import { Injectable } from '@angular/core';
+import { WorkItem } from 'TFS/WorkItemTracking/Contracts';
 
-import { WorkItem } from '../models';
+import * as m from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class WorkItemRepo {
-  public loadAllAsync(dateFieldName: string): Promise<WorkItem[]> {
+  public loadAllAsync(dateFieldName: string): Promise<m.WorkItem[]> {
     console.log(dateFieldName);
-    return new Promise((resolve: (value?: WorkItem[]) => void, _: any) => {
+    return new Promise((resolve: (value?: m.WorkItem[]) => void, _: any) => {
       VSS.require(['TFS/WorkItemTracking/RestClient'], (wit: any) => {
-        // tslint:disable-next-line: no-debugger
-        debugger;
         const client = wit.getClient();
-        client.getWorkItem(424).then((workItem: any) => {
+        client.getWorkItem(424).then((workItem: WorkItem) => {
           console.log(workItem);
         });
 
