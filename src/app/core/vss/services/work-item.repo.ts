79d3609987aced19f897590236cb0807a0b8ea61
Matthias Müller <MatthiasM@Live.de>
2019-09-22 11:22:55 +0200

    WIP

--- src/app/core/vss/services/work-item.repo.ts
+++ src/app/core/vss/services/work-item.repo.ts
@@ -2,7 +2,6 @@
 /// <reference path="../../../../../node_modules/vss-web-extension-sdk/typings/VSS.SDK.d.ts" />.
 
 import { Injectable } from '@angular/core';
-import { CommonMethods4To5 } from 'TFS/WorkItemTracking/RestClient';
 
 import { WorkItem } from '../models';
 
@@ -13,7 +12,6 @@ import { WorkItem } from '../models';
 export class WorkItemRepo {
   public loadAllAsync(dateFieldName: string): Promise<WorkItem[]> {
     return new Promise((resolve: (value?: WorkItem[]) => void, _: any) => {
-
       VSS.require(
         ['ReleaseManagement/Core/RestClient',
           'ReleaseManagement/Core/Contracts'], (TFS_RM_RestClient: any, contracts: any) => {
@@ -24,25 +22,29 @@ export class WorkItemRepo {
                   null,
                   contracts.ReleaseDefinitionExpands.Environments);
 
+              console.log(tra);
+
+              // tslint:disable-next-line: no-debugger
+              resolve([]);
               // tslint:disable-next-line: no-debugger
               debugger;
-              console.log(tra);
               // tslint:disable-next-line: no-debugger
             } catch (er) {
-              // tslint:disable-next-line: no-debugger
-              debugger;
               console.log(er);
             }
           });
 
-      VSS.require(['VSS/Service', 'TFS/WorkItemTracking/RestClient'], async (VSS_Service: any, TFS_Wit_WebApi: any) => {
+      VSS.require(['TFS/WorkItemTracking/RestClient'], (restClientWi: any) => {
         try {
-          const witClient = <CommonMethods4To5>VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
-          witClient.getWorkItems([424, 1074]).then(wis => {
+          // tslint:disable-next-line: no-debugger
+          debugger;
+          const witClient = restClientWi.getClient();
+          witClient.getWorkItem(424, 1074).then((wi: any) => {
             // tslint:disable-next-line: no-debugger
             debugger;
-            const workItems = wis.map(nw => new WorkItem(nw.id, nw.fields['title'], nw.fields['date']));
-            resolve(workItems);
+            console.log(wi);
+            // const workItems = wi.map(nw => new WorkItem(nw.id, nw.fields['title'], nw.fields['date']));
+            // resolve(workItems);
           });
           // tslint:disable-next-line: no-debugger
 
@@ -51,8 +53,29 @@ export class WorkItemRepo {
           debugger;
           console.log(er);
         }
-
       });
+
+      // VSS.require(['VSS/Service', 'TFS/WorkItemTracking/RestClient'], async (VSS_Service: any, TFS_Wit_WebApi: any) => {
+      //   try {
+      //     const witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+      //     witClient.getWorkItem(424, 1074).then((wi: any) => {
+      //       // tslint:disable-next-line: no-debugger
+      //       debugger;
+      //       console.log(wi);
+      //       // const workItems = wi.map(nw => new WorkItem(nw.id, nw.fields['title'], nw.fields['date']));
+      //       // resolve(workItems);
+      //     });
+      //     // tslint:disable-next-line: no-debugger
+
+      //   } catch (er) {
+      //     // tslint:disable-next-line: no-debugger
+      //     debugger;
+      //     console.log(er);
+      //   }
+      // });
+
+
+
 
       console.log(dateFieldName);
       // return [
