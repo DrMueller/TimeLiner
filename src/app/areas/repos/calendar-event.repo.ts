516eaed6ac5a20b77fb5a69239335990d75f8816
Matthias Müller <matthias.mueller@trivadis.com>
2019-09-23 16:25:58 +0200

    WIP

--- src/app/areas/repos/calendar-event.repo.ts
+++ src/app/areas/repos/calendar-event.repo.ts
@@ -24,11 +24,11 @@ export class CalendarEventRepo {
     // tslint:disable-next-line: no-debugger
     debugger;
 
-    const date = <Date>deadline!.value;
-    const beginDate = new Date(date.getDate());
+    const date = <string>deadline!.value;
+    const beginDate = new Date(date);
     beginDate.setUTCHours(0, 0, 0, 0);
 
-    const endDate = new Date(date.getDate());
+    const endDate = new Date(date);
     endDate.setUTCHours(25, 59, 59, 999);
 
     return new CalendarEvent(wi.id, title!.value, beginDate, endDate);
--- src/app/core/vss/data/queries/query.repo.ts
+++ src/app/core/vss/data/queries/query.repo.ts
@@ -19,7 +19,7 @@ export class QueryRepo {
 
   public async loadByProjectAsync(projectId: string): Promise<Query[]> {
     const client = await this.proxyFactory.createWorkItemTrackingClientAsync();
-    const nativeQueries = await client.getQueries(projectId, 3);
+    const nativeQueries = await client.getQueries(projectId, 3, 10, false);
     // tslint:disable-next-line: no-debugger
     debugger;
     const queries = nativeQueries.map(nativeQuery => this.adapter.adapt(nativeQuery));
