3520a700b5d28277ac64db5f1940f6701a4c4ac1
Matthias Müller <matthias.mueller@trivadis.com>
2019-09-24 08:12:15 +0200

    WIP

--- src/app/areas/components/config/config.component.ts
+++ src/app/areas/components/config/config.component.ts
@@ -8,23 +8,29 @@ import { SearchConfiguration } from '../../models';
   styleUrls: ['./config.component.scss']
 })
 export class ConfigComponent {
+  public set workTemDateFieldName(value: string) {
+    this._workItemDateFieldName = value;
+    this.omitChange();
+  }
+
   @Output() public searchConfigChanged = new EventEmitter<SearchConfiguration>();
   @Output() public dataRefreshRequested = new EventEmitter();
 
   private _workItemDateFieldName: string;
   private _queryId: string;
 
-  public set workTemDateFieldName(value: string) {
-    this._workItemDateFieldName = value;
-    this.omitChange();
-  }
 
   public selectedQueryIdChanged(queryId: string): void {
     this._queryId = queryId;
     this.omitChange();
   }
 
+  public refresh(): void {
+    this.dataRefreshRequested.emit();
+  }
+
   private omitChange(): void {
     this.searchConfigChanged.emit(new SearchConfiguration(this._queryId, this._workItemDateFieldName));
   }
+
 }
