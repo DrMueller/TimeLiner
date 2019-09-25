7713ab7a99185a0e2fd9ad6404006746fc0003ce
Matthias Müller <matthias.mueller@trivadis.com>
2019-09-25 11:36:08 +0200

    WIP

--- src/app/shell/app-main/app.module.ts
+++ src/app/shell/app-main/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { AboutComponent } from 'src/app/areas/components/about';
 import { CalendarComponent } from 'src/app/areas/components/calendar';
 import { ConfigComponent } from 'src/app/areas/components/config';
 import { OverviewComponent } from 'src/app/areas/components/overview';
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    AboutComponent,
     AppComponent,
     OverviewComponent,
     CalendarComponent,
